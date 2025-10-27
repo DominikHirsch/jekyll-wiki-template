6662e188133ed24b923ccf38963c2ed34c173d51
DominikHirsch <DominikHirsch@users.noreply.github.com>
2025-10-27 12:40:45 +0000

    deploy: 35a2e8d0a142be4a47f2e8002cb08a01c1c37d10

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,24 +1,24 @@
 var store = [{
         "title": "Seite 1",
-        "excerpt":"Seite 1   Das ist Seite 1 im Wiki.  ","categories": [],
+        "excerpt":"Das ist Seite 1 im Wiki.  ","categories": [],
         "tags": [],
         "url": "/jekyll-wiki-template/seite1/",
         "teaser": null
       },{
         "title": "Seite 2",
-        "excerpt":"Seite 2   Dies ist Seite 2 im Wiki.  Sie hat zwei Unterseiten.  ","categories": [],
+        "excerpt":"Dies ist Seite 2 im Wiki.  Sie hat zwei Unterseiten.  ","categories": [],
         "tags": [],
         "url": "/jekyll-wiki-template/seite2/",
         "teaser": null
       },{
         "title": "Unterseite 1",
-        "excerpt":"Unterseite 1   Das ist die erste Unterseite.  ","categories": [],
+        "excerpt":"Das ist die erste Unterseite von Seite 2.  ","categories": [],
         "tags": [],
         "url": "/jekyll-wiki-template/seite2/sub1/",
         "teaser": null
       },{
         "title": "Unterseite 2",
-        "excerpt":"Unterseite 2   Das ist die zweite Unterseite.  ","categories": [],
+        "excerpt":"Das ist die zweite Unterseite von Seite 2.  ","categories": [],
         "tags": [],
         "url": "/jekyll-wiki-template/seite2/sub2/",
         "teaser": null
